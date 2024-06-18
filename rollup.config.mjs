86e57acfb71fb199e31efc26b2547d64d76dbf3e
Oliver Cox <github@konx.dev>
2024-06-18 01:22:44 +0100

    eod - rollup issues

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,6 +2,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import dts from 'rollup-plugin-dts';
+import postcss from 'rollup-plugin-postcss';
 
 import packageJson from './package.json' assert { type: 'json' };
 
@@ -20,7 +21,7 @@ export default [
         sourcemap: true
       }
     ],
-    plugins: [resolve(), commonjs(), typescript({ tsconfig: './tsconfig.json' })]
+    plugins: [resolve(), commonjs(), typescript({ tsconfig: './tsconfig.json' }), postcss()]
   },
   {
     input: 'dist/esm/types/index.d.ts',
